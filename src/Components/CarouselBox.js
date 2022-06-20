4b6ae802dae2894e05cf96db3802ec62243146cd
vlad-doc-g <gorelickov.vladislav@yandex.ru>
2022-06-20 12:19:22 +0300

    'homecarousel'

--- src/Components/CarouselBox.js
+++ src/Components/CarouselBox.js
@@ -1,19 +1,64 @@
 import React, { Component } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
-import ant from '../assets/ant.jpeg'
+import tna1500mo from '../assets/tna1500mo.jpg'
+import rt70 from '../assets/rt70.jpg'
+import rtf32 from '../assets/rtf32.jpg'
+import tna1500 from '../assets/tna1500.jpg'
+import tna400 from '../assets/tna400.jpg'
+
 
 export default class CarouselBox extends Component {
     render() {
         return(
             <Carousel>
-                <Carousel.Item>
+                <Carousel.Item className="carousel-home">
                     <img 
                         className="d-block w-100"
-                        src={ant}
+                        src={tna1500mo}
                         alt='антенна'
                     />
                     <Carousel.Caption>
-                        <h3>картинка</h3>
+                        <h3>ТНА-1500 Медвежьи Озёра</h3>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item className="carousel-home">
+                    <img 
+                        className="d-block w-100"
+                        src={rt70}
+                        alt='антенна'
+                    />
+                    <Carousel.Caption>
+                        <h3>РТ-70</h3>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item className="carousel-home">
+                    <img 
+                        className="d-block w-100"
+                        src={rtf32}
+                        alt='антенна'
+                    />
+                    <Carousel.Caption>
+                        <h3>РТФ-32</h3>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item className="carousel-home">
+                    <img 
+                        className="d-block w-100"
+                        src={tna1500}
+                        alt='антенна'
+                    />
+                    <Carousel.Caption>
+                        <h3>ТНА-1500 Калязин</h3>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item className="carousel-home">
+                    <img 
+                        className="d-block w-100"
+                        src={tna400}
+                        alt='антенна'
+                    />
+                    <Carousel.Caption>
+                        <h3>ТНА-400</h3>
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,23 +1,32 @@
 import React, {Component} from 'react'
 import video from '../assets/antv.mp4'
+import CarouselBox from '../Components/CarouselBox'
 
 export default class Home extends Component {
     render() {
         return (
-            <div class="back-video-container">
-                <video
-                    controls 
-                    autoPlay
-                    loop
-                    muted
-                    class='back-video'
-                >
-                    <source src={video} type="video/mp4"/>
-                </video>
-                <div class='back-video-text-container'>
-                    <div class="back-video-text">29 ЛЕТ УСПЕШНОЙ РАБОТЫ!</div>
+            <>
+                <div class="back-video-container">
+                    <video
+                        controls
+                        autoPlay
+                        loop
+                        muted
+                        class='back-video'
+                    >
+                        <source src={video} type="video/mp4" />
+                    </video>
+                    <div class='back-video-text-container'>
+                        <div class="back-video-text">29 ЛЕТ УСПЕШНОЙ РАБОТЫ!</div>
+                    </div>
                 </div>
-            </div>
+                <div class="home-textb">
+                    <p>Мы принимали участие в строительстве таких объектов, как:</p>
+                </div>
+                <div>
+                    <CarouselBox />
+                </div>
+            </>
         )
     }
 }
