3934275d90217f975542e445d4608678551a7577
vlad-doc-g <gorelickov.vladislav@yandex.ru>
2022-06-22 17:27:15 +0300

    fixes

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -9,7 +9,7 @@ export default class Footer extends Component {
                 <div class="footer">
                     <Navbar collapseOnSelect>
                         <Container fluid>
-                            <Navbar.Brand href="/">
+                            <div className="devby">
                                 <img
                                     src={logo}
                                     height="30"
@@ -17,24 +17,8 @@ export default class Footer extends Component {
                                     className="d-inlone-block align-top"
                                     alt="Logo"
                                 />
-                            </Navbar.Brand>
-                            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                            <Navbar.Collapse id="responsive-navbar-nav">
-                                <Nav
-                                    fill
-                                    variant="tabs"
-                                    defaultActiveKey="/home"
-                                    className="mr-auto"
-                                >
-                                    <Nav.Link href="/development"> Разработка </Nav.Link>
-                                    <Nav.Link href="/works"> Работы </Nav.Link>
-                                    <Nav.Link href="/history"> История </Nav.Link>
-                                    <Nav.Link href="/geography"> География </Nav.Link>
-                                    <Nav.Link href="/contacts"> Контакты </Nav.Link>
-                                </Nav>
-                            </Navbar.Collapse>
-                            <div className="devby">developed by vlad.doc </div>
-                            <div>ООО "Спецмонтаж" 1993-2022. Все права защищены.</div>
+                                developed by vlad.doc
+                            </div>
                         </Container>
                     </Navbar>
                 </div>
